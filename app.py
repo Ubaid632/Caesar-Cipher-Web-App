2a5d39f40f20b6da5e4420f270b266f631970aa0
Ubaid632 <nubaida02@gmail.com>
2024-12-21 13:07:59 -0500

    app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def caesar(message, offset):
     return encrypted_text
 
 # Streamlit app layout
-st.title("secert code genarater")
+st.title("Caesar Cipher Web App")
 st.write("Encrypt your text using the Caesar cipher.")
 
 # Input for plain text
